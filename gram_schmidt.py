2c1fb677789270f0552102b85c5178c1a9d38ae5
legendongary <legendongary@gmail.com>
2019-09-19 15:58:16 +0800

    Create gram_schmidt.py

---
+++ gram_schmidt.py
@@ -0,0 +1,31 @@
+import torch
+
+
+def gram_schmidt(vv):
+    def projection(u, v):
+        return (v * u).sum() / (u * u).sum() * u
+
+    nk = vv.size(0)
+    uu = torch.zeros_like(vv, device=vv.device)
+    uu[:, 0] = vv[:, 0].clone()
+    for k in range(1, nk):
+        vk = vv[k].clone()
+        uk = 0
+        for j in range(0, k):
+            uj = uu[:, j].clone()
+            uk = uk + projection(uj, vk)
+        uu[:, k] = vk - uk
+    for k in range(nk):
+        uk = uu[:, k].clone()
+        uu[:, k] = uk / uk.norm()
+    return uu
+
+
+if __name__ == '__main__':
+    torch.autograd.set_detect_anomaly(True)
+    a = torch.randn(5, 5, requires_grad=True)
+    b = gram_schmidt(a)
+    c = b.sum()
+    c.backward()
+    print(b.matmul(b.t()))
+    print(a.grad)
